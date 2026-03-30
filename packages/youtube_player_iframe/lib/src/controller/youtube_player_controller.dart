602adf4d1bab6083085dd25049bfd5ec7d019ae0
cas-cm <casmedia123@gmail.com>
2026-03-30 15:55:36 +0530

    Update youtube_player_controller.dart

--- packages/youtube_player_iframe/lib/src/controller/youtube_player_controller.dart
+++ packages/youtube_player_iframe/lib/src/controller/youtube_player_controller.dart
@@ -322,7 +322,7 @@ class YoutubePlayerController implements YoutubePlayerIFrameAPI {
 
   Future<String> _prepareData(Map<String, dynamic>? data) async {
     await _eventHandler.isReady;
-    
+    print(data);
     if (data != null && data.containsKey('videoId')) {
       final String rawId = data['videoId'];
       // If it's not already quoted, wrap it in single quotes
